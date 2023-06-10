5ca28a1b5151d786b247d8a69972d87d45d5c035
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-06-10 13:35:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 var keys = document.querySelectorAll('.key');
 
 for (var i = 0; i < keys.length; i++) {
-  keys[i].addEventListener('keydown', function(e) {
+  keys[i].addEventListener('click', function(e) {
+	  // alert('he');
     var key = e.keyCode;
-    var audio, video = document.getElementById('sound');
-    audio.src = `https://github.com/acciojob/Drum-Kit-Nihal00/blob/master/sounds/boom.wav`;
+    var audio = document.getElementById('sound');
+    audio.src = `https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3`;
     audio.play();
   });
 }
