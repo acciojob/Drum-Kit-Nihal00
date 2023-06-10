456360afd81ab8aa41d34899ab686d1d3125c309
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-06-10 13:31:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var keys = document.querySelectorAll('.key');
 for (var i = 0; i < keys.length; i++) {
   keys[i].addEventListener('keydown', function(e) {
     var key = e.keyCode;
-    var audio = document.getElementById('sound');
+    var audio, video = document.getElementById('sound');
     audio.src = `https://github.com/acciojob/Drum-Kit-Nihal00/blob/master/sounds/boom.wav`;
     audio.play();
   });
