0675d5608338a1c24276df829c36db2fbf8765e6
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-06-10 13:26:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ for (var i = 0; i < keys.length; i++) {
   keys[i].addEventListener('keydown', function(e) {
     var key = e.keyCode;
     var audio = document.getElementById('sound');
-    audio.src = `https://pixabay.com/sound-effects/search/clap/`;
+    audio.src = `https://pixabay.com/sound-effects/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=6697;
     audio.play();
   });
 }
