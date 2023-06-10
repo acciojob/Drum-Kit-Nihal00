b6e009221061e8ed3aaa00aea0aede19bce40592
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-06-10 13:40:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 var keys = document.querySelectorAll('.key');
 
 for (var i = 0; i < keys.length; i++) {
-  keys[i].addEventListener('click', function(e) {
+  keys[i].addEventListener('keydown', function(e) {
 	  // alert('he');
     var key = e.keyCode;
     var audio = document.getElementById('sound');
-    audio.src = `https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3`;
+    audio.src = `/main.html`;
     audio.play();
   });
 }
