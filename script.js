f22395974e742cd65a03c99b249fe0e99307939c
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-06-10 13:17:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+var keys = document.querySelectorAll('.key');
+
+for (var i = 0; i < keys.length; i++) {
+  keys[i].addEventListener('keydown', function(e) {
+    var key = e.keyCode;
+    var audio = document.getElementById('sound');
+    audio.src = `/sounds/${key}.mp3`;
+    audio.play();
+  });
+}
